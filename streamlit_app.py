8d5f8e2b86087d8fcb52afcbe694ec5d913c53fe
Kalavari Shiva Kumar <134154341+ShivaKumarKalavari@users.noreply.github.com>
2025-02-08 12:46:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import requests
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 # Download the LSTM model from GitHub
